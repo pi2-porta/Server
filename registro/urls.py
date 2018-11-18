d5faee50da8a0e1c3e0729d854814d76344f7a77
rafaelbrg <rabragancaunb@gmail.com>
2018-11-18 20:09:35 -0200

    envio funcionando

--- registro/urls.py
+++ registro/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from .views import ListCreateRegistrosView
 from .views import LoginView
 from .views import RegisterUsersView
+from .views import SearchUserPhotoView
 
 
 urlpatterns = [
     path('registros/', ListCreateRegistrosView.as_view(), name="registros-all"),
+    path('search/', SearchUserPhotoView.as_view(), name="search" ),
     path('auth/login/', LoginView.as_view(), name="auth-login"),
     path('auth/register/', RegisterUsersView.as_view(), name="auth-register")
+
 ]
