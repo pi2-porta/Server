538a204b6d88a05d942f38bb095c25a46221fce8
andersonms1 <andersoonn.ams@gmail.com>
2018-11-26 00:11:23 -0200

    add voice

---
+++ txt_to_speech.py
@@ -0,0 +1,10 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say('Greetings!')
+engine.say('Anderson')
+engine.say('How are you today?')
+engine.runAndWait()
+
+
+# pip3 install pyttsx3
+# sudo apt-get update && sudo apt-get install espeak
